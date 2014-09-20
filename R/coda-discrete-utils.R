351cce529e262f179637761512ee7a71a77607ae
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2014-09-20 11:12:43 -0700

    First working version

--- R/coda-discrete-utils.R
+++ R/coda-discrete-utils.R
@@ -1,3 +1,18 @@
-coda.pmf <- function(variable.data) {
-  freq(variable.data)
+coda.pmf <- function(coda.output) {
+  my.matrix <- as.matrix(mcmc.output)
+  
+  for (col.name in colnames(my.matrix)) 
+  {
+    my.freq <- freq(variable.data, plot=FALSE)
+    number.rows <- dim(my.freq)[1]
+    my.freq <- my.freq[-number.rows,]
+    pdf(paste(col.name,"-pmf.pdf",sep=""))
+    x.values <- as.numeric(row.names(my.freq))
+    y.values <- my.freq[,2]/100
+    max.y <- max(y.values)
+    plot(x.values,y.values,type="h",xlab="k",ylab=paste("Approximation of P(",col.name,"= k)",sep=""),main=NULL,ylim=c(0,max.y)) 
+    title("Probability Mass Function (PMF)")
+    garbage <- dev.off();
+  }
 }
+
